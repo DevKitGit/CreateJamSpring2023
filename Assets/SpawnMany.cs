44e83c15e7f3f81858655b5ddca8dc30a71c1c82
Kit <me@devkit.dk>
2023-04-16 08:49:28 +0200

    OceanRes upgraded

--- Assets/SpawnMany.cs
+++ Assets/SpawnMany.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.XR.CoreUtils;
 using UnityEditor;
 using UnityEngine;
@@ -9,24 +10,26 @@ public class SpawnMany : MonoBehaviour
     [SerializeField] private GameObject prefab;
     
     [SerializeField] private Vector2Int dimensions;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Spawn();
-    }
+    [SerializeField] private float scale;
 
+    
     // Update is called once per frame
     [ContextMenu("Spawn things")]
     private void Spawn()
     {
+        var transforms = transform.GetComponentsInChildren<Transform>().Skip(1).ToArray();
+        for (var i = 0; i < transforms.Length; i++)
+        {
+            DestroyImmediate(transforms[i].gameObject);
+        }
         var middlepos = gameObject.transform.position;
-        var localscale = gameObject.transform.localScale;
+        var localscale = new Vector3(scale, 1f, scale);
         for (int x = -dimensions.x; x < dimensions.x; x++)
         {
             for (int y = -dimensions.y; y < dimensions.y; y++)
             {
-                Instantiate(prefab, middlepos + new Vector3(x * 10 * localscale.x, 0, y * 10 * localscale.z), Quaternion.identity,transform);
-                
+                var go =Instantiate(prefab, middlepos + new Vector3(x * 10 * localscale.x, 0, y * 10 * localscale.z), Quaternion.identity,transform);
+                go.transform.localScale = localscale;
             }
         }
     }
